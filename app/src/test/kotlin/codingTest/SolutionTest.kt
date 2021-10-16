3b7a0d9938f5dd0902066616eb7d18fbfc71e920
raeperd <raeperd117@gmail.com>
2021-10-16 11:11:31 +0900

    Configure Solution and SoutionTest First

---
+++ app/src/test/kotlin/codingTest/SolutionTest.kt
@@ -0,0 +1,17 @@
+package codingTest
+
+import org.junit.Test
+import org.assertj.core.api.Assertions.assertThat
+
+internal class SolutionTest {
+
+    private val solution = Solution()
+
+    @Test
+    fun example_test() {
+    }
+
+    @Test
+    fun example_test_2() {
+    }
+}
