ef2bc92d5f154d3d542648f7eb965afb1150b1b6
raeperd <raeperd117@gmail.com>
2021-12-17 13:31:22 +0900

    #1 Add parameterized test codes

--- src/test/kotlin/SolutionTest.kt
+++ src/test/kotlin/SolutionTest.kt
@@ -1,12 +1,27 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class SolutionTest {
+
+    companion object {
+        @JvmStatic
+        fun provideTestCase() = listOf(1 to 1)
+            .map { Arguments.of(it.first, it.second) }
+    }
 
     private val solution = Solution()
 
     @Test
     fun solutionTest() {
         assertThat(solution.solution()).isEqualTo(1)
+    }
+
+    @MethodSource("provideTestCase")
+    @ParameterizedTest
+    fun example_parameterized_test(value: Int, expected: Int) {
+        assertThat(value).isEqualTo(expected)
     }
 }
